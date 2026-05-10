371dc682de9e7f4d4c440d3135338e19d7d068e4
vipul agrawal <48291129+vipulagr890@users.noreply.github.com>
2026-05-11 03:41:51 +0530

    chore: clean up test files

--- jarvis_daemon/test_inherit.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    std::process::Command::new("pi").args(["--help"]).stdin(std::process::Stdio::inherit()).stdout(std::process::Stdio::inherit()).stderr(std::process::Stdio::inherit()).spawn().unwrap().wait().unwrap();
-}
